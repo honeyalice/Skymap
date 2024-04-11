24487c74dc25cb1ed0a02ece23ff106d4cda269c
beingjiang <96422819+beingjiang@users.noreply.github.com>
2024-04-11 01:10:38 -0400

    chang

--- demo/celestial.js
+++ demo/celestial.js
@@ -1002,8 +1002,8 @@ Celestial.display = function(config) {
  
 //Export entire object if invoked by require
 if (typeof module === "object" && module.exports) {
-  var d3js = require('./lib/d3.js'),
-      d3_geo_projection = require('./lib/d3.geo.projection.js');
+  var d3js = require('../lib/d3.js'),
+      d3_geo_projection = require('../lib/d3.geo.projection.js');
   module.exports = {
     Celestial: function() { return Celestial; },
     d3: function() { return d3js; },
